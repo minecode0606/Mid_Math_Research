6a50308bfca285871eb14fec6201cfefa8039b9b
Minseokang <minseo_0606@naver.com>
2021-07-11 14:22:06 +0900

    (fix) Improved the problem that the previous execution record remains when the method is executed redundantly.

--- Pyscientific/mathematics/number/binary/Decimal_to_binary.py
+++ Pyscientific/mathematics/number/binary/Decimal_to_binary.py
@@ -1,56 +1,29 @@
-#class Store_the_list_index():
-#    def __init__(self):
-#        self.retls = []
-#    def storelist(self, element):
-#        self.retls.append(element)
-#        print(self.retls)
-
-# returnls = []
-# def return_total_list_element(ls : list):
-#     st = ""
-#     for i in list(map(str, ls)):
-#         st += str(i)
-#         print(i)
-#     return st
-# def delete0(ls : list):
-#     outlist = list(map(int, ls))
-#     index = 0
-#     for i in outlist:
-#         if i == 1:
-#             break
-#         else:
-#             outlist.pop(0)
-#     # outlist.pop(0)
-#     return outlist
-# class Decimal_to_binary():
-#     def __init__(self):
-#         raise NotImplemented # stopship 2021/07/10
-#
-#     def dectobin(self, num, digit = 8):
-#         dtb = Decimal_to_binary()
-#         digit = digit - 1
-#         if digit > 0:
-#             dtb.dectobin(num // 2, digit)
-#         returnls.append(num % 2)
-#         return return_total_list_element(delete0(returnls))
-#         # self.decobin_total_list.append(num % 2)
-#         # return self.decobin_total_list
-# # stopship 2021/07/10
-
-class Decimal_to_binary():
-    decimalls = []
+class Decimal_to_binary:
     @classmethod
-    def count_dectobin(cls, num, digit = 8):
+    def count_dectobin(cls, num, digit=8):
+        """
+        This function converts the given decimal number
+        to binary and stores it as a list in the decimalls
+        variable.
+        :param num:
+        :param digit:
+        :return type : none:
+        """
         digit = digit - 1
         if digit > 0:
             Decimal_to_binary.count_dectobin(num // 2, digit)
-        Decimal_to_binary.decimalls.append(num % 2)
+        try:
+            Decimal_to_binary.decimalls.append(num % 2)
+        except AttributeError:
+            raise TypeError("not before use the dectobin() staticmethod")
+
     @classmethod
     def return_total_list_element(cls, ls: list):
         st = ""
         for i in list(map(str, ls)):
             st += str(i)
         return st
+
     @classmethod
     def remove_first_0_element(cls, ls: list):
         outlist = list(map(int, ls))
@@ -63,7 +36,10 @@ class Decimal_to_binary():
                 break
         return returnls[index:len(returnls)]
 
-    def dectobin(self, num):
+
+    @staticmethod
+    def dectobin(num):
+        Decimal_to_binary.decimalls = []
         Decimal_to_binary.count_dectobin(int(num))
         return Decimal_to_binary.return_total_list_element(Decimal_to_binary.remove_first_0_element(Decimal_to_binary.decimalls))
 
